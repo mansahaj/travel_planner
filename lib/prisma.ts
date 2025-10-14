bab907ddfaecb8a87639b264391846574e16ff77
Mansahaj <msspopli@gmail.com>
2025-10-14 16:41:07 -0700

    added github authentication

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
